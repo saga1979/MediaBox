475a464135ba84cb5b94263625b5a55a4c5863bd
saga <zhuimeng79@msn.com>
2017-01-14 12:19:49 +0800

    需要系统支持的很简陋的swf播放器: 1、只能够播放swf（需要浏览器支持flash播放） 2、保存播放列表到固定文件 3、从固定文件加载播放列表

---
+++ MediaBox/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace MediaBox
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PlayItemDataList m_playItemList = new PlayItemDataList();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Binding binding = new Binding();
+            binding.Source = m_playItemList;
+            binding.Path = new PropertyPath("ListItems");
+            m_playListView.SetBinding(ItemsControl.ItemsSourceProperty, binding);
+
+        }
+
+        private void OnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.ShowDialog();
+            var dir = dialog.SelectedPath;
+            if (dir.Length == 0)
+            {
+                return;
+            }
+            m_playItemList.Clear();
+ 
+            foreach (var file in Directory.GetFiles(dir, "*.swf", SearchOption.AllDirectories))
+            {
+                PlayItemData item = new PlayItemData(file);
+                m_playItemList.AddItem(item);
+            }
+
+          
+        }
+
+        private void OnPlaylist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+            m_player.Source = new Uri(m_playItemList.GetItem((string)m_playListView.SelectedItem).Uri);
+        }
+
+        private void OnSave_PlayList(object sender, RoutedEventArgs e)
+        {
+            if(m_playItemList.Count == 0)
+            {
+                return;
+            }
+
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = Encoding.UTF8;
+            settings.Indent = true;
+            using (XmlWriter writer = XmlWriter.Create(".\\playlist.xml", settings))
+            {
+                writer.WriteStartElement("zf");
+                foreach(var file in m_playItemList.Items)
+                {
+                    writer.WriteElementString("item", file.Uri);
+                }
+            }
+        }
+
+        private void OnOpenPlayList(object sender, RoutedEventArgs e)
+        {            
+            ICollection<string> files = new List<string>();
+            using (XmlReader reader = XmlReader.Create(".\\playlist.xml"))
+            {
+                reader.MoveToContent();
+                reader.ReadStartElement("zf");
+                while(reader.Read())
+                {
+                    string s = reader.ReadInnerXml();
+                    files.Add(s);
+                }
+            }
+            if(files.Count == 0)
+            {
+                return;
+            }
+            m_playItemList.Clear();
+
+            m_playItemList.AddItems(files);
+        }
+    }
+
+}
---
+++ MediaBox/PlayItemDataList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBox
+{
+    class PlayItemData : System.ComponentModel.INotifyPropertyChanged
+    {
+        string m_path;
+        public PlayItemData(String path)
+        {
+            m_path = path;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return m_path.Substring(m_path.LastIndexOf("\\") +1);
+            }
+            
+        }
+        public string Uri
+        {
+            get
+            {
+                if(m_path.StartsWith("\\\\127.0.0.1\\"))
+                {
+                    return m_path;
+                }
+                string local = m_path.Remove(0, 3);
+                string new_file = local.Insert(0, "\\\\127.0.0.1\\" + m_path.Substring(0, 1) + "$\\");
+                return new_file;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    class PlayItemDataList : INotifyPropertyChanged
+    {
+        List<PlayItemData> m_list = new List<PlayItemData>();
+       
+        public List<String> ListItems
+
+        {
+            get
+            {
+                List<String> listItems = new List<string>();
+                foreach(var item in m_list)
+                {
+                    listItems.Add(item.Name);
+                }
+                return listItems;
+                
+            }
+        }
+
+        public List<PlayItemData> Items
+        {
+            get
+            {
+                return m_list;
+            }            
+        }
+
+        public PlayItemData GetItem(string name)
+        {
+            return m_list.Find(x => x.Name == name);
+        }
+
+        public int Count
+        {
+            get
+            {
+                return m_list.Count;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void AddItem(PlayItemData item)
+        {
+            m_list.Add(item);
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("ListItems"));
+        }
+
+        public void AddItems(ICollection<String> uris)
+        {
+            foreach(var uri in uris)
+            {
+                PlayItemData item = new PlayItemData(uri);
+                m_list.Add(item);
+            }
+
+            if(uris.Count != 0 && PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("ListItems"));
+            }
+        }
+
+        public void Clear()
+        {
+            m_list.Clear();
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs("ListItems"));
+        }
+
+    }
+}
